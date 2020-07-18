01733d6cf6cc9b746f742df1f337ff3f6aee23eb
Maciej Bedra <bedra.maciej@gmail.com>
2020-07-18 12:43:08 +0200

    Configuration for single external display in bash automation script

--- manjaro/home/.sh/screen_layout.sh
+++ manjaro/home/.sh/screen_layout.sh
@@ -7,14 +7,18 @@
 # @author Maciej Bedra
 
 OEM="OEM"
-LV_OEM="LV < OEM"
-SELECTED_LAYOUT=$(echo -e "$OEM\n$LV_OEM" | rofi -lines 5 -dmenu)
+SINGLE_EXTERNAL="SingleExternal"
+LV_OEM="LeftVertical < OEM"
+SELECTED_LAYOUT=$(echo -e "$OEM\n$SINGLE_EXTERNAL\n$LV_OEM" | rofi -lines 5 -dmenu)
 
 if [[ ! -z "$SELECTED_LAYOUT" ]]
 then
 	if [[ "$SELECTED_LAYOUT" == "$OEM" ]]
 	then
 		xrandr --output eDP1 --primary --mode 1920x1080 --pos 0x0 --rotate normal --output DP1 --off --output DP2 --off --output HDMI1 --off --output HDMI2 --off --output VIRTUAL1 --off
+	elif [[ "$SELECTED_LAYOUT" == "$SINGLE_EXTERNAL" ]] 
+	then
+		xrandr --output eDP1 --off --output DP1 --off --output DP2 --primary --mode 1920x1080 --pos 0x0 --rotate normal --output HDMI1 --off --output HDMI2 --off --output VIRTUAL1 --off
 	elif [[ "$SELECTED_LAYOUT" == "$LV_OEM" ]]
 	then
 		xrandr --output eDP1 --primary --mode 1920x1080 --pos 1080x420 --rotate normal --output DP1 --mode 1920x1080 --pos 0x0 --rotate left --output DP2 --off --output HDMI1 --off --output HDMI2 --off --output VIRTUAL1 --off
